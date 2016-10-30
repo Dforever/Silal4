6068c425cd7a08333431b04096f3a175e04d016f
David Silal <dsilal@albany.edu>
2016-10-30 13:12:32 -0400

    Made changes to consturctor Stack

--- Stack.java
+++ Stack.java
@@ -1,67 +1,39 @@
 public class Stack {
-	private ListNode headNode;
+ private ListNode headNode;
 
 
 
 public Stack() {
-	headNode = null;
+ headNode = null;
 
 }
 
 //print the list in the forward direction
 public void print() {
-	ListNode element = headNode;
-	while (element != null)
-	{
-		System.out.print(element.getInfo());
-		if (element.getTail() != null)
-			System.out.print(" <= ");
-		element = element.getTail();
-	}
-	System.out.print("\n");
+ ListNode element = headNode;
+ while (element != null)
+ {
+  System.out.print(element.getInfo());
+  if (element.getTail() != null)
+   System.out.print(" <= ");
+  element = element.getTail();
+ }
+ System.out.print("\n");
 }
 
 
 
 //Find the size of the list
 public int size() {
-	ListNode element = headNode;
-	int count = 0;
-	//go through each element
-	while (element != null)
-	{
-		count = count + 1;
-		element = element.getTail();
-	}
-	return count;
+ ListNode element = headNode;
+ int count = 0;
+ //go through each element
+ while (element != null)
+ {
+  count = count + 1;
+  element = element.getTail();
+ }
+ return count;
 }
 
-public String peek() {
-	if (headNode == null)
-		return null;
-		return headNode.getInfo();
-	}
 
-public void push(String item) {
-	ListNode newNode = new ListNode(item);
-	if (headNode == null) {
-		headNode = newNode;
-	} else {
-		newNode.setTail(headNode);
-		headNode = newNode;
-	}
-
-}
-
-public String pop() {
-		if (headNode == null) {
-			return null;
-		} else {
-			String curr = headNode.getInfo();
-			headNode = headNode.getTail();
-			return curr;
-
-		}
-}
-
-}
