b789d0acd59c4b03f9b2f424b5d8d3add56bff6d
David Silal <dsilal@albany.edu>
2016-10-30 13:18:31 -0400

    Created constrcutor Queue

---
+++ MyQueue.java
@@ -0,0 +1,40 @@
+public class MyQueue {
+ private ListNode headNode;
+ private ListNode tailNode;
+
+
+
+public MyQueue() {
+ headNode = null;
+ tailNode = null;
+
+}
+
+//print the list in the forward direction
+public void print() {
+ ListNode element = headNode;
+ while (element != null)
+ {
+  System.out.print(element.getInfo());
+  if (element.getTail() != null)
+   System.out.print(" <= ");
+  element = element.getTail();
+ }
+ System.out.print("\n");
+}
+
+
+
+//Find the size of the list
+public int size() {
+ ListNode element = headNode;
+ int count = 0;
+ //go through each element
+ while (element != null)
+ {
+  count = count + 1;
+  element = element.getTail();
+ }
+ return count;
+}
+
