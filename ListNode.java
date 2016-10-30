626ef6cd64b50ff7918cf34936459ed63f393bd2
David Silal <dsilal@albany.edu>
2016-10-30 13:09:33 -0400

    Took getters and setters out for headNode

--- ListNode.java
+++ ListNode.java
@@ -1,26 +1,16 @@
 //This is derived from the link(https://github.com/csi-cen-213) on the CSI-213CLASS github
 public class ListNode {
 	//local variables
-	private ListNode head;
+
 	private String info;
 	private ListNode tail;
 
 	//This constructor contains just solely data
 	public ListNode(String Info) {
-		head = null;
+
 		info = Info;
 		tail = null;
 
-	}
-
-	//get head
-	public ListNode getHead() {
-		return head;
-	}
-
-	//set the head node
-	public void setHead(ListNode NewHead) {
-		head = NewHead;
 	}
 	//get tail
 	public ListNode getTail() {
